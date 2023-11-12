bfead353c7714458ecc3b92c1c15643c3b1b335c
cgrosmax <gros.charlesmax@gmail.com>
2023-11-12 13:16:58 -0500

    mise a jour

--- js/portfolio.js
+++ js/portfolio.js
@@ -17,40 +17,40 @@ function closePopup(popupId) {
   
 
 
-  function enregistrerDonnees() {
-    // Récupérez les valeurs des champs du formulaire
-    const nom = document.getElementById("nom").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
+  // function enregistrerDonnees() {
+  //   // Récupérez les valeurs des champs du formulaire
+  //   const nom = document.getElementById("nom").value;
+  //   const email = document.getElementById("email").value;
+  //   const message = document.getElementById("message").value;
   
-    // Créez un objet JSON avec les données du formulaire
-    const donnees = {
-      nom: nom,
-      email: email,
-      message: message,
-    };
+  //   // Créez un objet JSON avec les données du formulaire
+  //   const donnees = {
+  //     nom: nom,
+  //     email: email,
+  //     message: message,
+  //   };
   
-    // Convertissez l'objet JSON en une chaîne JSON
-    const donneesJSON = JSON.stringify(donnees);
+  //   // Convertissez l'objet JSON en une chaîne JSON
+  //   const donneesJSON = JSON.stringify(donnees);
   
-    // Créez un objet Blob contenant le JSON
-    const blob = new Blob([donneesJSON], { type: "application/json" });
+  //   // Créez un objet Blob contenant le JSON
+  //   const blob = new Blob([donneesJSON], { type: "application/json" });
   
-    // Créez un lien pour le téléchargement du fichier JSON
-    const a = document.createElement("a");
-    a.href = URL.createObjectURL(blob);
-    a.download = "donnees.json";
-    a.textContent = "Télécharger le fichier JSON";
+  //   // Créez un lien pour le téléchargement du fichier JSON
+  //   const a = document.createElement("a");
+  //   a.href = URL.createObjectURL(blob);
+  //   a.download = "donnees.json";
+  //   a.textContent = "Télécharger le fichier JSON";
   
-    // Ajoutez le lien à la page
-    document.body.appendChild(a);
+  //   // Ajoutez le lien à la page
+  //   document.body.appendChild(a);
   
-    // Cliquez automatiquement sur le lien pour déclencher le téléchargement
-    a.click();
+  //   // Cliquez automatiquement sur le lien pour déclencher le téléchargement
+  //   a.click();
   
-    // Supprimez le lien après le téléchargement
-    document.body.removeChild(a);
-  }
+  //   // Supprimez le lien après le téléchargement
+  //   document.body.removeChild(a);
+  // }
 
   
   
